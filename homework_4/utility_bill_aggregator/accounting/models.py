7531bea261524d60ffeb2f24e582efaeb6ba2470
Elena Mashchenko <mashchenko_01@mail.ru>
2021-11-21 20:07:43 +0300

    Add homework 5

--- homework_4/utility_bill_aggregator/accounting/models.py
+++ homework_4/utility_bill_aggregator/accounting/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    type = models.CharField(max_length=64, verbose_name='Тип')
+    organization = models.CharField(max_length=64, verbose_name='Организация')
+    address = models.TextField(verbose_name='Адрес организации', null=True)
+
+class Account(models.Model):
+    MONTHS = [
+        ('January', 'Январь'),
+        ('February', 'Февраль'),
+        ('March', 'Март'),
+        ('April', 'Апрель'),
+        ('May', 'Май'),
+        ('June', 'Июнь'),
+        ('July', 'Июль'),
+        ('August', 'Август'),
+        ('September', 'Сентябрь'),
+        ('October', 'Октябрь'),
+        ('November', 'Ноябрь'),
+        ('December', 'Декабрь')
+    ]
+    personal_account = models.CharField(max_length=15, verbose_name='Лицевой счет')
+    mounth =  models.CharField(max_length=10, verbose_name='Месяц', choices=MONTHS)
+    year = models.CharField(max_length=4, verbose_name='Год')
+    payment_amount = models.FloatField(verbose_name='Сумма платежа')
+    indications = models.TextField(verbose_name='Показания счетчиков', null=True)
+    electronic_receipt_filename = models.TextField(verbose_name='Название файла с электронной квитанцией', null=True)
+    address = models.TextField(verbose_name='Адрес квартиры', null=True) 
+    paid_for = models.BooleanField(verbose_name='Оплачен', default=False)
+    service_type = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name='Тип платежа', null=True)
+
--- homework_4/utility_bill_aggregator/application/settings.py
+++ homework_4/utility_bill_aggregator/application/settings.py
@@ -72,17 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'application.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -125,3 +114,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
